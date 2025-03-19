594896f92ac22796e7da46401ecbfbac3900cb35
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-19 19:31:45 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  name_prefix = "${var.tags}-elasticache"
+  name_prefix = "${var.env}-elasticache"
   tags        = merge(var.tags, { Name = "tf-module-elasticache" }, { env = var.env })
 }
